69b2c8e55235d40868e100c8b112726479c230df
projectsbyph <philipj@live.se>
2026-01-08 09:53:36 +0100

    first commit

---
+++ DSO25_TEST_INTRO_XUNIT/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSO25_TEST_INTRO_XUNIT
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return 0;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return 0;
+        }
+    }
+}
---
+++ DSO25_TEST_INTRO_XUNIT/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+namespace DSO25_TEST_INTRO_XUNIT
+{
+    public class CalculatorTest
+    {
+
+        [Fact]
+        public void TestAdd()
+        {
+            //given
+            Calculator calculator = new Calculator(); // Skapar en instans av Calculator-klassen
+            int a = 5;
+            int b = 3;
+            int expected = 8;
+
+            //when
+            int result = calculator.Add(a, b); // Anropar Add-metoden med värdena a och b
+
+            //then
+            Assert.Equal(expected, result); // Denna rad kontrollerar att resultatet är som förväntat
+        }
+    }
+}
